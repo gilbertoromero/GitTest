e916b7bd95b7b925b4df1956696e280564aa2247
gilbertoromero <cronos256@gmail.com>
2014-09-17 12:05:12 -0700

    added a test class

---
+++ GTest.h
@@ -0,0 +1,13 @@
+//
+//  GTest.h
+//  Git Test
+//
+//  Created by GAMELOFT on 9/17/14.
+//  Copyright (c) 2014 Gameloft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GTest : NSObject
+
+@end
---
+++ GTest.m
@@ -0,0 +1,13 @@
+    //
+//  GTest.m
+//  Git Test
+//
+//  Created by GAMELOFT on 9/17/14.
+//  Copyright (c) 2014 Gameloft. All rights reserved.
+//
+
+#import "GTest.h"
+
+@implementation GTest
+
+@end
